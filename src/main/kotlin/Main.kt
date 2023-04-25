8ea2f1ec3d25abf01face5b8afdccd10c63ded59
lincoln pires <lincoln.ufop@gmail.com>
2023-04-25 14:08:29 -0300

    knapSack recursion dp

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ fun main(args: Array<String>) {
     println("Program arguments: ${args.joinToString()}")
 
     var div = 3.toDouble() / 2.toDouble()
-    println("Valor div: ${div}")
+    println("Valor div: $div")
 
     val generatedArray = IntArray(10) { it }
     generatedArray.forEach { print(it) }
@@ -22,4 +22,12 @@ fun main(args: Array<String>) {
     val intArray3 = arrayOfNulls<Int>(5)
     println("size: ${intArray3.size}")
     intArray3.forEach { print(it) }
+
+    println()
+    val rows = 3
+    val cols = 4
+    val matrix = Array(rows) { IntArray(cols) }
+    for (row in matrix) {
+        println(row.contentToString())
+    }
 }
--- src/main/kotlin/dynamicprogramming/KnapsackProblem01DP.kt
+++ src/main/kotlin/dynamicprogramming/KnapsackProblem01DP.kt
@@ -1,6 +1,43 @@
 package dynamicprogramming
 
-//https://www.geeksforgeeks.org/printing-items-01-knapsack/
-//https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
+// https://www.geeksforgeeks.org/printing-items-01-knapsack/
+// https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
+// https://www.youtube.com/watch?v=vLLIU2UQN-o
 
+fun knapSackDP(capacityOfKnapsack: Int, weights: IntArray, profits: IntArray, numberOfItems: Int): Int {
+    var knapsackTable = Array(numberOfItems + 1) { IntArray(capacityOfKnapsack + 1) }
+    knapsackTable.forEach {
+        it.forEach { print("|$it") }
+        println()
+    }
 
+    var totalBenefit = 0
+
+    for (i in 1 until numberOfItems + 1) {
+        for (weight in 1 until capacityOfKnapsack + 1) {
+            var notTakingItem = knapsackTable[i - 1][weight]
+            println("Not Taking: ${notTakingItem}")
+            var takingItem = 0
+
+            if (i < numberOfItems && weights[i] <= weight) {
+                takingItem = profits[i] + knapsackTable[i - 1][weight - weights[i]]
+                println("TakingItem: ${takingItem}")
+            }
+            knapsackTable[i][weight] = Math.max(notTakingItem, takingItem)
+            knapsackTable.forEach {
+                it.forEach { print("|$it") }
+                println()
+            }
+        }
+        totalBenefit = knapsackTable[numberOfItems][capacityOfKnapsack]
+    }
+    return totalBenefit
+}
+
+fun main() {
+    var profits = intArrayOf(60, 100, 120)
+    var weights = intArrayOf(1, 2, 3)
+    var capacity = 5
+    var numberOfItens = profits.size
+    println(knapSackDP(capacity, weights, profits, numberOfItens))
+}
